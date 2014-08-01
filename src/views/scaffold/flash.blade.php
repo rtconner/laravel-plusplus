144d36d28a9bf9593a996cb7bf79f1ebfddad304
rtconner <rtconner@gmail.com>
2014-08-01 19:29:53 -0400

    add flash scaffolds

---
+++ src/views/scaffold/flash.blade.php
@@ -0,0 +1,36 @@
+@if(\Session::has('success'))
+	
+ <div class="alert alert-success alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	{{{ \Session::get('success') }}}
+</div>
+
+@elseif(\Session::has('error'))
+
+<div class="alert alert-danger alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	{{{  \Session::get('error') }}}
+</div>
+
+@elseif(\Session::has('message'))
+
+<div class="alert alert-info alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	{{{  \Session::get('message') }}}
+</div>
+
+@elseif(\Session::has('info'))
+
+<div class="alert alert-info alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	{{{  \Session::get('info') }}}
+</div>
+
+@elseif(\Session::has('warning'))
+
+<div class="alert alert-warning alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	{{{  \Session::get('warning') }}}
+</div>
+
+@endif
---
+++ src/views/scaffold/flash/humane/flatty.blade.php
@@ -0,0 +1,31 @@
+@if(\Session::has('success'))
+	
+	@section('scripts')
+	<script>humane.log("{{{ jse(\Session::get('success')) }}}", { timeout: 5000, addnCls: 'humane-flatty-success' })</script>
+	@stop
+	
+@elseif(\Session::has('error'))
+
+	@section('scripts')
+	<script>humane.log("{{{ jse(\Session::get('error')) }}}", { timeout: 5000, addnCls: 'humane-flatty-error' })</script>
+	@stop
+
+@elseif(\Session::has('message'))
+
+	@section('scripts')
+	<script>humane.log("{{{ jse(\Session::get('message')) }}}", { timeout: 5000 })</script>
+	@stop
+
+@elseif(\Session::has('info'))
+
+	@section('scripts')
+	<script>humane.log("{{{ jse(\Session::get('info')) }}}", { timeout: 5000 })</script>
+	@stop
+
+@elseif(\Session::has('warning'))
+
+	@section('scripts')
+	<script>humane.log("{{{ jse(\Session::get('warning')) }}}", { timeout: 5000, addnCls: 'humane-flatty-error' })</script>
+	@stop
+	
+@endif
