47dfcdc6ceb53c580b19716be23438967db81d25
rtconner <rtconner@gmail.com>
2014-08-04 01:31:59 -0400

    update owned trait

--- src/Conner/PlusPlus/OwnedTrait.php
+++ src/Conner/PlusPlus/OwnedTrait.php
@@ -5,5 +5,11 @@ trait OwnedTrait {
 	public function scopeOwned($query) {
 		return $query->where('user_id', user('id'));
 	}
+
+	public static function bootOwnedTrait() {
+		static::registerModelEvent('creating', function($model){
+			$model->user_id = user('id');
+		});
+	}
 	
 }
