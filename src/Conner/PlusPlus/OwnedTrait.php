c3393b89e8779158c1e6ff145d44881637079184
rtconner <rtconner@gmail.com>
2014-08-04 12:08:49 -0400

    update owned trait

--- src/Conner/PlusPlus/OwnedTrait.php
+++ src/Conner/PlusPlus/OwnedTrait.php
@@ -2,14 +2,32 @@
 
 trait OwnedTrait {
 
+	/**
+	 * Save user_id when new records are created
+	 */
+	public static function bootOwnedTrait() {
+		static::registerModelEvent('creating', function($model){
+			if(empty($model->user_id)) {
+				$model->user_id = user('id');
+			}
+		});
+	}
+
+	/**
+	 * Build query to only find records owned by currently logged in user
+	 */
 	public function scopeOwned($query) {
 		return $query->where('user_id', user('id'));
 	}
-
-	public static function bootOwnedTrait() {
-		static::registerModelEvent('creating', function($model){
-			$model->user_id = user('id');
-		});
+	
+	/**
+	 * Is the currently logged in user the owner
+	 */
+	public function getAmOwnerAttribute() {
+		$userId = user('id');
+		if(empty($userId)) { return false; }
+		
+		return $userId == $this->attributes['user_id'];
 	}
 	
 }
