dad636eab1557d948b8f6e910d0632fd66d3c035
rtconner <rtconner@gmail.com>
2014-08-04 00:28:31 -0400

    changing error structure a little

---
+++ src/views/errors.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+
+if(!empty($errors) && get_class($errors) == 'Illuminate\Support\ViewErrorBag') {
+	
+	$messages = $errors->all();
+	
+	if($messages) {
+		echo '<div class="alert alert-danger alert-dismissable">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
+				.implode('<br>', $messages)
+				.'</div>';
+	};
+	
+}
